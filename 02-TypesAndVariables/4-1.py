a7b7e7f681191bf54028cafb5c042964e6b2ecae
Vladyslav Kondrat <236127@student.uek.krakow.pl>
2024-10-11 10:17:31 +0200

    4.1 done, 4.2 needs some work

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,8 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+age = "18"
+height = "175"
+future_age = "24"
+print(f"My name is {name}. I am {age} years old, and my height is {height} cm. In 6 years I will be {future_age} years old.")
