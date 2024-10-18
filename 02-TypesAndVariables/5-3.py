140c6efddd70178b4a7a0a137eb9e92777422882
Vladyslav Kondrat <236127@student.uek.krakow.pl>
2024-10-18 14:03:39 +0200

    Багато нових завдань

---
+++ 02-TypesAndVariables/5-3.py
@@ -0,0 +1,8 @@
+###
+# A program that calculates the volume
+# and surface area of ​​a cuboid with sides a, b, and c.
+# Read the dimensions of the cuboid from the keyboard.
+#
+a = input('a=')
+...
+...
